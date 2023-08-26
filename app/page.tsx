7c94ee6b2f8778611d2a77583cd6c7e968bb5f4d
Daniel Dominique <danmeldominique@gmail.com>
2023-08-26 23:27:25 +1000

    Add desktop design

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,16 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="h-screen md:bg-charcoalGray text-charcoalGray">
+    <main className="h-screen md:bg-charcoalGray text-charcoalGray md:flex md:justify-center md:items-center">
       {/* container */}
-      <section className="flex flex-col relative h-full">
+      <section className="flex flex-col md:flex-row relative h-full md:w-fit md:mx-auto md:h-2/3 md:bg-white md:justify-center md:rounded-[1rem] md:p-[1rem] md:shadow-lg md:drop-shadow-lg">
         {/* Image */}
-      <div className="h-48 overflow-hidden rounded-b-[1rem] ">
+      <div className="h-48 md:h-full overflow-hidden rounded-b-[1rem] md:rounded-[1rem]">
         <Image src={MobileImage} alt="Sign up" className='md:hidden object-cover min-w-full' />
+        <Image src={DesktopImage} alt="Sign up" className='hidden md:block object-cover min-w-full min-h-full' />
       </div>
       {/* Main content */}
-      <div className="flex flex-col h-full px-6">
+      <div className="flex flex-col h-full px-6 md:order-first">
         <h1 className="font-bold text-4xl mt-8">Stay updated!</h1>
         <div className="my-auto text-sm">
           <p className="my-4"> Join 60,000+ product managers receiving monthly updates on:</p>
